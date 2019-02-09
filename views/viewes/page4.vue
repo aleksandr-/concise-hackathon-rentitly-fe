72bc8a453efece154e712226d21c8a1e7241c780
Martti Mikk <martti.mikk@easypark.net>
2019-02-09 16:38:43 +0200

    some more fields

--- views/viewes/page4.vue
+++ views/viewes/page4.vue
@@ -3,13 +3,13 @@
     <v-card class="elevation-2">
     <v-layout row xs12 pa-5>
       <v-layout column>
-          <h2>Your booking details</h2>
+        <h2>Your booking details</h2>
         <h3>Pickup date</h3>
         <h3>{{date}}</h3>
         <h3>Return date</h3>
         <h3>{{dateTo}}</h3>
-
-
+        <h3>Price: {{item.price}}</h3>
+        <h3>(for 1 day)</h3>
         </v-layout>
       <v-layout column>
 
@@ -20,6 +20,69 @@
         <h2>Name:</h2> <span style="font-size:24px; font-weight: 100; margin-left: 12px"> {{item.name}}</span>
         </v-layout>
         </v-layout>
+    </v-layout>
+    <v-layout row  pa-5>
+      <v-layout column xs6 m>
+        <v-layout row>
+          <v-text-field
+                  v-model="first"
+                  label="First Name"
+                  box
+                  style="margin-right: 20px"
+          ></v-text-field>
+        </v-layout>
+      </v-layout>
+      <v-layout column xs6>
+        <v-layout row>
+          <v-text-field
+                  v-model="first"
+                  label="Last Name"
+                  box
+          ></v-text-field>
+        </v-layout>
+      </v-layout>
+    </v-layout>
+    <v-layout row  pa-5>
+        <v-layout column xs6>
+          <v-layout row>
+            <v-text-field
+                    v-model="first"
+                    label="Email"
+                    box
+                    style="margin-right: 20px"
+            ></v-text-field>
+          </v-layout>
+        </v-layout>
+      <v-layout column xs6>
+        <v-layout row>
+          <v-text-field
+                  v-model="first"
+                  label="Confirm email"
+                  box
+          ></v-text-field>
+        </v-layout>
+      </v-layout>
+    </v-layout>
+      <v-layout row  pa-5>
+        <v-layout column xs6>
+
+            <v-select
+                    :items="items"
+                    box
+                    label="Box style"
+                    style="margin-right: 20px"
+            ></v-select>
+
+        </v-layout>
+      <v-layout column xs6>
+        <v-layout row>
+          <v-text-field
+                  v-model="first"
+                  label="Cardnumber"
+                  box
+          ></v-text-field>
+        </v-layout>
+      </v-layout>
     </v-layout>
     </v-card>
   </v-flex>
